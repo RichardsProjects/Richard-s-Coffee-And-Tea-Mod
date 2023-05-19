88a378e49f1f0d83caa0705b5d3d0c076a87cda3
richardb122 <richard@richardsprojects.net>
2023-05-19 18:50:55 -0400

    Fix bug with half tea cups

--- 1.19.3-1.19.4/java/net/richardsprojects/teamod/CoffeeAndTeaMod.java
+++ 1.19.3-1.19.4/java/net/richardsprojects/teamod/CoffeeAndTeaMod.java
@@ -57,7 +57,7 @@ public class CoffeeAndTeaMod
     public static final RegistryObject<Item> ITEM_BLOCK_HALF_COFFEE_SUGAR_CUP = ITEMS.register("half_coffee_sugar_cup", () -> new ItemBlockHalfCoffeeSugarCup());
     public static final RegistryObject<Item> ITEM_BLOCK_HALF_COFFEE_CUP = ITEMS.register("half_coffee_cup", () -> new ItemBlockHalfCoffeeCup());
     public static final RegistryObject<Item> ITEM_BLOCK_FULL_TEA_CUP = ITEMS.register("full_tea_cup", () -> new ItemBlockFullTeaCup());
-    public static final RegistryObject<Item> ITEM_BLOCK_HALF_TEA_CUP = ITEMS.register("half_tea_cup", () -> new ItemBlockFullTeaCup());
+    public static final RegistryObject<Item> ITEM_BLOCK_HALF_TEA_CUP = ITEMS.register("half_tea_cup", () -> new ItemBlockHalfTeaCup());
     public static final RegistryObject<Item> ITEM_BLOCK_MORTAR_AND_PESTLE = ITEMS.register("mortar_and_pestle", () -> new ItemBlockMortarAndPestle());
 
     private void addCreative(CreativeModeTabEvent.BuildContents event) {
